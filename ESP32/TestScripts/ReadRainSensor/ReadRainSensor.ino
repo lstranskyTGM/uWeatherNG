6a191c5eb70bb42504aff6e3181b49038ef8e1c3
lstranskyTGM <104894882+lstranskyTGM@users.noreply.github.com>
2023-05-16 10:15:14 +0200

    Update ReadRainSensor.ino
    Changed RainSensor Code

--- ESP32/TestScripts/ReadRainSensor/ReadRainSensor.ino
+++ ESP32/TestScripts/ReadRainSensor/ReadRainSensor.ino
@@ -2,18 +2,18 @@
 
 // for ESP32 Microcontroller
 #define rainAnalog 35
-#define rainDigital 34
+// #define rainDigital 34
 // #define sensorPower 7
 boolean bIsRaining;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
 }
 void loop() {
   // digitalWrite(sensorPower, HIGH);  // Turn the sensor ON
   // delay(10);              // Allow power to settle
   int rainAnalogVal = analogRead(rainAnalog);
-  int rainDigitalVal = digitalRead(rainDigital);
+  // int rainDigitalVal = digitalRead(rainDigital);
 
   /*
   if(rainAnalogVal < 100) {
@@ -26,14 +26,17 @@ void loop() {
   // digitalWrite(sensorPower, LOW);    // Turn the sensor OFF
 
   Serial.println(rainAnalogVal);
-
-  if (rainAnalogVal < 3000) {
-    Serial.println("Es regnet")
-  } else {
-    Serial.println("Es regnet nicht")
+  
+  if (!bIsRaining && rainAnalogVal < 2700) {
+    Serial.println("Es regnet");
+    bIsRaining = true;
+  } else if (bIsRaining && rainAnalogVal > 3400) {
+    Serial.println("Es regnet nicht");
+    bIsRaining = false;
   }
+
   // Serial.print("\t");
-  Serial.println("Digital: "+rainDigitalVal); // No value gets printed
+  // Serial.println("Digital: "+rainDigitalVal); // No value gets printed
   delay(1000); 
 }
 
